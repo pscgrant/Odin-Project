46c464fbadf8c16c6182f132034c794df202488e
Sean Grant <pscgrant95@gmail.com>
2022-10-10 14:09:11 -0700

    practice

--- foundations/JS Fundamentals/part3/functions.js
+++ foundations/JS Fundamentals/part3/functions.js
@@ -1,3 +1,9 @@
 //https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Functions
 
 
+
+        // let add7 = (number) => number + 7;
+        // console.log(number);
+let number = 5;
+let add7 = (number) => number + 7;
+console.log(number);
