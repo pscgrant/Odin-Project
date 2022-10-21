0bc6263972239d2c52c42c92d859c760a15b413a
Sean Grant <pscgrant95@gmail.com>
2022-10-21 12:14:27 -0700

    notes for next function

--- foundations/JS Fundamentals/Rock-Paper-Scissors/script.js
+++ foundations/JS Fundamentals/Rock-Paper-Scissors/script.js
@@ -1,7 +1,7 @@
 /*
-1. 
+This function is for the computer to randomly choose between
+rock, paper and scissor. Using a if else statement.
 */
-
  function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber == 1) {
@@ -12,3 +12,9 @@
         return "Scissors";
     }
 };
+
+
+/* This function will run the game.
+Taking two parameters the playerSelection and computerSelction,
+and returning a string that declares the winner */
+
