1595182cbabdb3bf7ee0198be08d0993aeddd277
Sean Grant <pscgrant95@gmail.com>
2022-10-27 13:57:52 -0700

    playRound function with if and else if statements

--- foundations/JS Fundamentals/Rock-Paper-Scissors/script.js
+++ foundations/JS Fundamentals/Rock-Paper-Scissors/script.js
@@ -19,5 +19,25 @@ Taking two parameters the playerSelection and computerSelection,
 and returning a string that declares the winner */
 
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Scissors") {
+            return "Rock wins! " + "You beat the computer."
+        } else {
+            return "Paper wins! " + "Sorry the computer beat you."
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            return "Paper wins! " + "You beat the computer."
+        } else {
+            return "Scissors wins! " + "Sorry the computer beat you."
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            return "Rock wins! " + "Sorry the computer beat you."
+        } else {
+            return "Scissors win! " + "You beat the computer."
+        }
+    }
 }
