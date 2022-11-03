5fc548e0face50fe496fbdeb06aa11ebdff3c294
Sean Grant <pscgrant95@gmail.com>
2022-11-03 16:48:01 -0700

    uncommented orginal function

--- foundations/JS Fundamentals/Rock-Paper-Scissors/script.js
+++ foundations/JS Fundamentals/Rock-Paper-Scissors/script.js
@@ -24,52 +24,52 @@ and returning a string that declares the winner */
 
 //This is the first attempt at the function creating 
 
-// function playRound(playerSelection, computerSelection) {
-//     if (playerSelection === computerSelection) {
-//         return "It's a tie!"
-//     } else if (playerSelection === "Rock".toLocaleLowerCase()) {
-//         if (computerSelection === "Scissors".toLocaleLowerCase()) {
-//             return "Rock wins! " + "You beat the computer."
-//         } else {
-//             return "Paper wins! " + "Sorry the computer beat you."
-//         }
-//     } else if (playerSelection === "Paper".toLocaleLowerCase()) {
-//         if (computerSelection === "Rock".toLocaleLowerCase()) {
-//             return "Paper wins! " + "You beat the computer."
-//         } else {
-//             return "Scissors wins! " + "Sorry the computer beat you."
-//         }
-//     } else if (playerSelection === "Scissors".toLocaleLowerCase()) {
-//         if (computerSelection === "Rock".toLocaleLowerCase()) {
-//             return "Rock wins! " + "Sorry the computer beat you."
-//         } else {
-//             return "Scissors win! " + "You beat the computer."
-//         }
-//     }
-// };
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        // roundWinner = "It's a tie!"
         return "It's a tie!"
-    } else if (
-        (playerSelection === "Rock".toLocaleLowerCase && computerSelection === "Scissors".toLocaleLowerCase) || 
-        (playerSelection === "Scissors".toLocaleLowerCase && computerSelection === "Paper".toLocaleLowerCase) ||
-        (playerSelection === "Paper".toLocaleLowerCase && computerSelection === "Rock".toLocaleLowerCase)
-    ) {
-        playerScore++
-        // roundWinner = "player"
-        return "Player wins!"
-    } else if (
-        (computerSelection === "Rock".toLocaleLowerCase && playerSelection === "Scissors".toLocaleLowerCase) ||
-        (computerSelection === "Scissors".toLocaleLowerCase && playerSelection === "Paper".toLocaleLowerCase) ||
-        (computerSelection === "Paper".toLocaleLowerCase && playerSelection === "Rock".toLocaleLowerCase)
-    ) {
-        computerScore++
-        // roundWinner = "computer"
-        return "Computer wins!"
+    } else if (playerSelection === "Rock".toLocaleLowerCase()) {
+        if (computerSelection === "Scissors".toLocaleLowerCase()) {
+            return "Rock wins! " + "You beat the computer."
+        } else {
+            return "Paper wins! " + "Sorry the computer beat you."
+        }
+    } else if (playerSelection === "Paper".toLocaleLowerCase()) {
+        if (computerSelection === "Rock".toLocaleLowerCase()) {
+            return "Paper wins! " + "You beat the computer."
+        } else {
+            return "Scissors wins! " + "Sorry the computer beat you."
+        }
+    } else if (playerSelection === "Scissors".toLocaleLowerCase()) {
+        if (computerSelection === "Rock".toLocaleLowerCase()) {
+            return "Rock wins! " + "Sorry the computer beat you."
+        } else {
+            return "Scissors win! " + "You beat the computer."
+        }
     }
-}
+};
+
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection === computerSelection) {
+//         // roundWinner = "It's a tie!"
+//         return "It's a tie!"
+//     } else if (
+//         (playerSelection === "Rock".toLocaleLowerCase && computerSelection === "Scissors".toLocaleLowerCase) || 
+//         (playerSelection === "Scissors".toLocaleLowerCase && computerSelection === "Paper".toLocaleLowerCase) ||
+//         (playerSelection === "Paper".toLocaleLowerCase && computerSelection === "Rock".toLocaleLowerCase)
+//     ) {
+//         playerScore++
+//         // roundWinner = "player"
+//         return "Player wins!"
+//     } else if (
+//         (computerSelection === "Rock".toLocaleLowerCase && playerSelection === "Scissors".toLocaleLowerCase) ||
+//         (computerSelection === "Scissors".toLocaleLowerCase && playerSelection === "Paper".toLocaleLowerCase) ||
+//         (computerSelection === "Paper".toLocaleLowerCase && playerSelection === "Rock".toLocaleLowerCase)
+//     ) {
+//         computerScore++
+//         // roundWinner = "computer"
+//         return "Computer wins!"
+//     }
+// }
 
 const playerSelection = prompt("Enter your selection of Rock, Paper or Scissors");
 const computerSelection = getComputerChoice();
