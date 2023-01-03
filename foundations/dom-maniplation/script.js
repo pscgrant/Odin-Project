5dee36c7d64443dbf1a3d7eb44d130b16bbc4260
Sean Grant <pscgrant95@gmail.com>
2023-01-03 10:33:33 -0800

    created P text with style through DOM

--- foundations/dom-maniplation/script.js
+++ foundations/dom-maniplation/script.js
@@ -5,3 +5,8 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const para = document.createElement('p')
+para.textContent = 'Hey, I\'m red.';
+para.style.color = 'red';
+container.appendChild(para);
