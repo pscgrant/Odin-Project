521f892be47abeb044365d1b3eb66e7301b2c69c
Sean Grant <pscgrant95@gmail.com>
2023-01-03 17:23:11 -0800

    btn with event listener

--- foundations/dom-maniplation/script.js
+++ foundations/dom-maniplation/script.js
@@ -31,3 +31,8 @@ container.appendChild(moreContent);
 
 const btn = document.querySelector('#btn');
 btn.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+    alert("Hello World");
+});
